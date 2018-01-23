a88a6f09c45111119c2942b29f89b5ac3142bcac
Muhammad Althaf <althf.m@gmail.com>
2018-01-23 18:08:31 +0530

    Updated env configs

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  COSMIC_BUCKET: JSON.stringify(process.env.COSMIC_BUCKET)
+  NODE_ENV: '"development"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  COSMIC_BUCKET: JSON.stringify(process.env.COSMIC_BUCKET)
+  COSMIC_BUCKET: `"${process.env.COSMIC_BUCKET}"`,
+  COSMIC_READ_KEY: `"${process.env.COSMIC_READ_KEY}"`,
+  COSMIC_WRITE_KEY: `"${process.env.COSMIC_WRITE_KEY}"`
 }
