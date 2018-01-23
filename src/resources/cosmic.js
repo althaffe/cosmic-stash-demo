26724f43595d525a60a8d8bd27b52d296cb3210e
Muhammad Althaf <althf.m@gmail.com>
2018-01-23 20:27:07 +0530

    added write key in bucket config

--- src/resources/cosmic.js
+++ src/resources/cosmic.js
@@ -2,7 +2,8 @@ const Cosmic = require('cosmicjs')()
 
 const bucket = Cosmic.bucket({
   slug: process.env.COSMIC_BUCKET,
-  read_key: process.env.COSMIC_READ_KEY
+  read_key: process.env.COSMIC_READ_KEY,
+  write_key: process.env.COSMIC_WRITE_KEY,
 })
 
 export default bucket
